0f0bce19657ee1d30478f49b687c045e72bfdae0
Fares00T <62139647+Fares00T@users.noreply.github.com>
2022-04-07 02:03:13 +0100

    ajax

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { reset } = require("nodemon");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.set("trust proxy", 1); // trust first proxy
 app.use(
@@ -28,31 +28,24 @@ const port = 3000;
 
 app.get("/home", (req, res) => {
   fs.readFile(__dirname + "/book.txt", (err, data) => {
-    let booksData = JSON.parse(data.toString());
-    let userData = JSON.parse(data.toString());
-    res.render("pages/home", { book: booksData, name: userData });
+    res.sendFile(__dirname + "/home.html");
   });
 });
 
 app.get("/", (req, res) => {
-  res.render("pages/log");
+  res.sendFile(__dirname + "/log.html");
 });
 
 app.get("/addBook", (req, res) => {
-  res.render("pages/addBook");
+  res.sendFile(__dirname + "/addBook.html");
 });
 
-app.get("/register", (req, res) => {
-  res.render("pages/register");
-});
-
-app.post("/addBook", function (req, res) {
+app.post("/addBooktext", function (req, res) {
   console.log(req.body);
-  res.redirect("/home");
   fs.readFile(__dirname + "/book.txt", (err, data) => {
-    const d = JSON.parse(data.toString());
-    d.push(req.body);
-    fs.writeFile(__dirname + "/book.txt", JSON.stringify(d), () => {
+    const bookdata = JSON.parse(data.toString());
+    bookdata.push(req.body);
+    fs.writeFile(__dirname + "/book.txt", JSON.stringify(bookdata), () => {
       console.log("success");
     });
   });
@@ -60,36 +53,50 @@ app.post("/addBook", function (req, res) {
 
 app.post("/register", function (req, res) {
   console.log(req.body);
-
   fs.readFile(__dirname + "/user.txt", (err, data) => {
     const userData = JSON.parse(data.toString());
     userData.push(req.body);
     fs.writeFile(__dirname + "/user.txt", JSON.stringify(userData), () => {
       console.log("success");
-      res.redirect("/");
+      res.send("user added");
     });
   });
 });
 
+app.get("/bookslist", (req, res) => {
+  fs.readFile(__dirname + "/book.txt", (err, data) => {
+    const booksdata = JSON.parse(data.toString());
+    res.status(200).json(booksdata);
+  });
+});
+
+app.get("/theName", (req, res) => {
+  fs.readFile(__dirname + "/user.txt", (err, data) => {
+    const theName = JSON.parse(data.toString());
+    res.status(200).json(theName);
+  });
+});
+
 app.post("/login", (req, res) => {
+  console.log(req.body);
   fs.readFile(__dirname + "/user.txt", (err, data) => {
     if (err) throw err;
     const content = JSON.parse(data.toString());
     const user = content.find(
       (user) =>
-        user.email === req.body["email"] &&
-        user.password === req.body["password"]
+        user.email === req.body["loginEmail"] &&
+        user.password === req.body["loginPassword"]
     );
     console.log(user);
     if (!user) {
-      res.redirect("/");
+      res.status(422).send("email or password is worng");
       console.log("email or password is worng");
       return;
     } else {
       req.session.username = user.username;
 
       console.log("username : " + req.session.username);
-      res.redirect("/home");
+      res.send("true");
       return;
     }
   });
